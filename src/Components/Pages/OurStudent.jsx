b50180b118c3e481ee6a107a2f13c10d7ed2f422
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-10 10:46:59 +0545

    adding search bar in our student

--- src/Components/Pages/OurStudent.jsx
+++ src/Components/Pages/OurStudent.jsx
@@ -10,9 +10,11 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { getAllStudent } from "../../services/api";
 import { Link } from "react-router-dom";
+import { useMemo } from "react";
 
 const OurStudent = () => {
   const [student, setStudent] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     getAllUsers();
@@ -23,35 +25,54 @@ const OurStudent = () => {
     setStudent(res.data);
   };
 
+  const filteredItems = useMemo(() => {
+    return student.filter((item) => {
+      if (item) {
+        return item.name.toString().toLowerCase().includes(query.toLowerCase());
+      }
+    });
+  }, [student, query]);
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>ID</TableCell>
-          <TableCell>Name</TableCell>
-          <TableCell>Address</TableCell>
-          <TableCell>Phone</TableCell>
-          <TableCell>Details </TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {student.map((val, i) => {
-          console.log(val);
-          return (
-            <TableRow key={i}>
-              <TableCell>{i}</TableCell>
-              <TableCell>{val.name}</TableCell>
-              <TableCell>{val.address}</TableCell>
-              <TableCell>{val.contact}</TableCell>
-              <Link to={`/single/${val._id}`}>
-                {" "}
-                <TableCell className="more">More...</TableCell>
-              </Link>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <div className="relative">
+      <input
+        type="search"
+        name="search"
+        placeholder="search a particular student"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className=" absolute top-1 text-xl right-1 mb-10 py-1 border-2 border-transparent shadow-xl"
+      />
+      <div className="pb-10 invisible ">hello</div>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Address</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Details </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredItems.map((val, i) => {
+            console.log(val);
+            return (
+              <TableRow key={i}>
+                <TableCell>{i}</TableCell>
+                <TableCell>{val.name}</TableCell>
+                <TableCell>{val.address}</TableCell>
+                <TableCell>{val.contact}</TableCell>
+                <Link to={`/single/${val._id}`}>
+                  {" "}
+                  <TableCell className="more">More...</TableCell>
+                </Link>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
